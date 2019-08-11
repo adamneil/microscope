d65aec2ead2695ad1a3023bc4c781ba99eb5b854
Jake <adam.falck@gmail.com>
2019-08-11 17:31:53 -0500

    added WSIWYG editor

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,9 +1,7 @@
 import { Template } from "meteor/templating";
 
 Template.postItem.helpers({
-    domain: function() {
-        var a = document.createElement('a');
-        a.href = this.url;
-        return a.hostname;
-    }
+    ownPost: function () {
+        return this.userId === Meteor.userId();
+    },
 });
---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,15 @@
+import SimpleSchema from 'simpl-schema';
+SimpleSchema.extendOptions(['autoform']);
+
+Template.postSubmit.events({
+    'submit form': function (e) {
+        e.preventDefault();
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val(),
+            text: $(e.target).find('[name=text]').val()
+        };
+        post._id = Posts.insert(post);
+        Router.go('postPage', post);
+    }
+});
