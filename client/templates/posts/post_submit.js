02ac833ae5a36aee52fd277e11b08e1b2c922293
Jake <adam.falck@gmail.com>
2019-08-13 10:21:00 -0500

    added image functionality with GridFS

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,21 +1,40 @@
 Template.postSubmit.events({
-    'submit form': function (e) {
-        e.preventDefault();
+  'submit form': function (e) {
+    e.preventDefault();
 
-        var post = {
-            title: $(e.target).find('[name=title]').val(),
-            text: $(e.target).find('[name=text]').val(),
-        };
+    var post = {
+      title: $(e.target).find('[name=title]').val(),
+      text: $(e.target).find('[name=text]').val(),
+      image: imagesURL
+    };
 
-        Meteor.call('postInsert', post, function (error, result) {
-            //display the error to the user and abort
-            if (error)
-                return alert(error.reason);
 
-            //show result but route anyway
-            if (result.postExits)
-                return ('This link has already been posted');
-        });
-        Router.go('postsList');
-    }
+
+    Meteor.call('postInsert', post, function (error, result) {
+      //display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+
+      //show result but route anyway
+      if (result.postExits)
+        return ('This link has already been posted');
+    });
+    Router.go('postsList');
+  },
+
 });
+
+Template.postSubmit.events({
+  'change .blogImage': function(event) {
+    FS.Utility.eachFile(event, function (file) {
+    Images.insert(file, function (err, fileObj) {
+      if (err) {
+        //handle error
+      } else {
+        // handle success
+        return imagesURL = "/cfs/files/images/" + fileObj._id
+      };
+    })
+  });
+}
+})
